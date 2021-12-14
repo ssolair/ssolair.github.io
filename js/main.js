bed5feb2efa64d16ef79ac0ce9a7f2b186d57787
0solair <66895894+0solair@users.noreply.github.com>
2021-12-14 18:46:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ function addImgsAll() { // Load all images with specified tags
         while (i < response.result.values.length) {
             // Add images to website
             console.log("grab")
-            if (comparelist(JSON.parse(response.result.values[i][1]), query, min_query)){
+            if (comparelist(response.result.values[i][1].slice(2, -2).split("', '"), query, min_query)){
                 LoadedImages += 1
                 if (LoadedImages > min && LoadedImages < max) {
                         if (response.result.values[i][0].match(/\.(mp4|webm|mov)$/)) { // To add support for another file extension that displays on a website, add |extensionhere after the last extension.
