8388fd1b5da655dde9de09ffc22dbb185a0a346e
0solair <66895894+0solair@users.noreply.github.com>
2021-12-14 21:13:26 -0500

    checks

--- js/main.js
+++ js/main.js
@@ -75,6 +75,7 @@ function addImgsAll() { // Load all images with specified tags
         }
         var file_nums = (page * 25)
         
+        console.log(file_nums)
 
         var query = document.getElementById('tags').value.split(', ');
         var min_query = [];
@@ -90,10 +91,12 @@ function addImgsAll() { // Load all images with specified tags
         
         var LoadedImages = 0
         while (i < response.result.values.length && LoadedImages != file_nums) {
+            console.log("while check")
             // Add images to website
             if (comparelist(response.result.values[i][1].slice(2, -2).split("', '"), query, min_query)){
-                
+                console.log("Compare check")
                 if (LoadedImages < file_nums && LoadedImages >= (file_nums-25)) {
+                    console.log("ifirst check")
                         if (response.result.values[i][0].match(/\.(mp4|webm|mov)$/)) { // To add support for another file extension that displays on a website, add |extensionhere after the last extension.
                             var imgs = document.createElement("video");
                             imgs.setAttribute("controls","controls")
@@ -109,7 +112,7 @@ function addImgsAll() { // Load all images with specified tags
                     imgs.style.border = "5px";
                     LoadedImages += 1
                 }           
-            }   
+            }
             i++;
         }
     }, function(reason) { // Obviously just print the error if there is one
