61cdbde4ccb52caa2d15fa1911e74b79df27b6f4
0solair <66895894+0solair@users.noreply.github.com>
2021-12-14 20:11:30 -0500

    Get back up to date my son

--- js/main.js
+++ js/main.js
@@ -16,9 +16,28 @@ function notify(msg, color) {
     setTimeout(() => {notifier.style.visibility='hidden'}, 5000);
 }
 
+function comparelist(taglist, list, min_list){
+    for (var a of list) {
+        if (!(taglist.includes(a))){
+            return false
+        }
+    }
+    for (var b of min_list) {
+        if (taglist.includes(b)) {
+            return false
+        }
+    }
+    return true
+}
 
+var input = document.getElementById("tags");
 
-
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("update-button").click();
+  }
+}); 
 
 ssID = "1NJrmjnYCJp-E4VJyQa3DmeZtm6-PwsFfkmyDBLrkLAw"
 
@@ -44,7 +63,7 @@ function addImgsAll() { // Load all images with specified tags
         spreadsheetId: ssID,
         range: 'Sheet1!A:B',
     };
-    
+
     var request = gapi.client.sheets.spreadsheets.values.get(params);
     request.then(function(response) {
         let i = 0;
@@ -57,46 +76,49 @@ function addImgsAll() { // Load all images with specified tags
         if (isNaN(max)) {
             max = 9999999
         }
-        var rows_data = new array(response.result.values.length)
-        for(let i = 0; i < rows_data.length; i++) {
-            rows_data[i] = {
-                url: response.result.values[i][0],
-                tags: response.result.velues[i][1].split(', ')
+        var query = document.getElementById('tags').value.split(', ');
+        var min_query = [];
+        for(let x = 0; x < query.length; x++){
+            if (query[x][0] == '-') {
+                min_query.push(query[x].substring(1));
+                query.splice(x, 1);
             }
         }
+        if (query[0] == "") {
+            query = []
+        }
+        
         while (i < response.result.values.length) {
             // Add images to website
-            function imgHandle() {
+            if (comparelist(response.result.values[i][1].slice(2, -2).split("', '"), query, min_query)){
                 LoadedImages += 1
                 if (LoadedImages > min && LoadedImages < max) {
-                    if (response.result.values[i][0].match(/\.(mp4|webm|mov)$/)) {
-                        var imgs = document.createElement("video");
-                        imgs.setAttribute("controls","controls")
-                        imgs.setAttribute("loop","true")
-                    } else {
-                        var imgs = document.createElement("img");
-                    }
-                    var src = document.getElementById("body");
+                        if (response.result.values[i][0].match(/\.(mp4|webm|mov)$/)) { // To add support for another file extension that displays on a website, add |extensionhere after the last extension.
+                            var imgs = document.createElement("video");
+                            imgs.setAttribute("controls","controls")
+                            imgs.setAttribute("loop","true")
+                        } else {
+                            var imgs = document.createElement("img");
+                        }
+                    var src = document.getElementById("body"); 
                     imgs.src = response.result.values[i][0];
                     src.appendChild(imgs);
-                    imgs.style.width = imgsize;
+                    imgs.style.width = '450px';
                     imgs.style.height = 'auto';
+                    imgs.style.border = "5px";
                 }
-            }
-            if (response.result.values[i][0] != null) {
-                imgHandle();
-            }
+            }   
 
             i++;
         }
-    }, function(reason) {
+    }, function(reason) { // Obviously just print the error if there is one
         console.error('error: ' + reason.result.error.message);
     });
 }
 
 
 
-function initClient() {
+function initClient() { // Runs right as the user enters the website.
     var API_KEY = 'AIzaSyA8v4fvkZwDNgqfICcvXxSAypOe1HWk_JM';
 
     var CLIENT_ID = '461958078181-kq3re2kbr613dg511su21e78ouvg8s29.apps.googleusercontent.com';
@@ -108,19 +130,9 @@ function initClient() {
     'clientId': CLIENT_ID,
     'scope': SCOPE,
     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(function() {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
+    })
 }
 
-function handleClientLoad() {
+function handleClientLoad() { // When a client loads the page
     gapi.load('client:auth2', initClient);
-}
-
-function updateSignInStatus(isSignedIn) {
-    if (isSignedIn) {
-    get();
-    notify("Signed in.", '#00744d');
-    }
 }
