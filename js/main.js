91d70e73f89b76a274d15c02a896d5f0a0f60a03
0solair <66895894+0solair@users.noreply.github.com>
2021-12-14 20:55:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,11 +90,11 @@ function addImgsAll() { // Load all images with specified tags
         }
         
 
-        while (i < response.result.values.length && i < file_nums) {
+        while (i < response.result.values.length && Loaded_images <= file_nums) {
             // Add images to website
             if (comparelist(response.result.values[i][1].slice(2, -2).split("', '"), query, min_query)){
-                LoadedImages += 1
-                if (LoadedImages <= file_nums && LoadedImages > (file_nums-25)) {
+                
+                if (LoadedImages > (file_nums-25)) {
                         if (response.result.values[i][0].match(/\.(mp4|webm|mov)$/)) { // To add support for another file extension that displays on a website, add |extensionhere after the last extension.
                             var imgs = document.createElement("video");
                             imgs.setAttribute("controls","controls")
@@ -109,8 +109,9 @@ function addImgsAll() { // Load all images with specified tags
                     imgs.style.height = 'auto';
                     imgs.style.border = "5px";
                 }
-                i++;
+                LoadedImages += 1              
             }   
+            i++;
         }
     }, function(reason) { // Obviously just print the error if there is one
         console.error('error: ' + reason.result.error.message);
