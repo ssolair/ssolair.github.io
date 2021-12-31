a1603cf93dcfa0cc1f36eb8502221a0cb22fa371
Solair <66895894+ssolair@users.noreply.github.com>
2021-12-30 22:27:25 -0500

    ????

--- js/main.js
+++ js/main.js
@@ -126,14 +126,15 @@ function addImgsAll() { // Load all images with specified tags
                 } else {
                     var imgs = document.createElement("img");
                 }
-            var src = document.getElementById("body"); 
-            imgs.src = files[i].url;
+            var src = document.getElementById("body");
             imgs.id = `img${i}`;
-            imgs.onclick = notify(imgs.src);
-            src.appendChild(imgs);
-            imgs.style.width = '450px';
-            imgs.style.height = 'auto';
-            imgs.style.border = "5px";
+            var image = document.getElementById(`img${i}`);
+            image.src = files[i].url;
+            image.onclick = notify(imgs.src);
+            src.appendChild(image);
+            image.style.width = '450px';
+            image.style.height = 'auto';
+            image.style.border = "5px";
             }else{
                 break
             }   
